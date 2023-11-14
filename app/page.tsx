9eb555b5d1e5667b682da8d32541e8f66e8fbeee
Taiyo Kohno <taiyowork66@gmail.com>
2023-11-14 23:45:53 +0900

    api chage

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { User } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 
 import { Container } from "@/components/Container/Container";
 import { Header } from "@/components/Header/Header";
 
 export default async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_PREFIX}${process.env.NEXT_PUBLIC_VERCEL_URL}/api/user`);
-  const users: User[] = await res?.json();
+  const users = await prisma.user.findMany();
 
   return (
     <div>
       <Header title="利用者選択画面" />
       <Container>
-        <div className="grid grid-cols-3 gap-4">
-          {users?.map((user) => (
+        <div className="grid grid-cols-4 gap-4">
+          {users.map((user) => (
             <Link href={`/user/${user.id}`} key={user.id}>
               <div className="flex flex-col gap-2 items-center p-4 shadow-md rounded-md">
-                <div className="w-full h-60 relative">
+                <div className="w-full h-72 relative">
                   <Image
                     src={user.image || "/images/user.png"}
                     alt={`${user.image}の画像`}
