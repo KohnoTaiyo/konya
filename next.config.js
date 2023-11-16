c88ffc4be947dc3944644695cc4836b605361812
Taiyo Kohno <taiyowork66@gmail.com>
2023-11-16 22:29:01 +0900

    CORS

--- next.config.js
+++ next.config.js
@@ -4,27 +4,27 @@ const nextConfig = {
     domains: ["pub-b6b1e243015545a985dc4623df615e21.r2.dev"],
   },
   experimental: { typedRoutes: true },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "app/api/:path*",
-  //       headers: [
-  //         {
-  //           key: "Access-Control-Allow-Origin",
-  //           value: "https://konya.vercel.app/",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "POST,PUT",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value: "Content-Type",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "app/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
